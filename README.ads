d009754f200317e472dd81a41f07993a45c050c3
Peter Teuben <teuben@gmail.com>
2018-01-20 23:58:04 -0500

    cleanup

--- README.ads
+++ README.ads
@@ -2,18 +2,22 @@
 See https://pdc-svpaap1.umd.edu/policies/apt_forms.html
 for the CVTemplate.docx.
 
-I had issues with this, perhaps LibreOffice,perhaps my inability
-to work with WYSIWYG programs.
+I had issues with this, perhaps LibreOffice, perhaps my inability
+to work with WYSIWYG programs:
+
+For example, in II I would try and add an item in II.A.3 and it
+doesn't get the indent, but the one before. The TAB key then doesn't
+seem to work, and skips over wehre you expect it go to.  and then the
+underlining is suddenly on.... is that a LibreOffice bug?
+I had many more of these mishaps, and decided to switch to the
+latex version.
+
 
 The docs template should also give an example how they expect
-the indentation to wokr, and the UID and name is confiusing.
+the indentation to work, and the UID and name is confusing.
 As given, the indentation in some sections DOES NOT MAKE SENSE.
 E.g. in I it works.
 
-But in II you would try and add an item in II.A.3 and it doesn't
-get the indent, but one before. The TAB key then doesn't seem
-to work, and skips over wehre you expectr it go to.  and then
-the underlining is suddently on.... is that a LibreOffice bug?
 
 
 ADS:
@@ -33,6 +37,7 @@ http://adsabs.harvard.edu/abs_doc/help_pages/customformat.html
  and postdoctoral and Faculty Research Assistants were co-authors.
 "
 
+
 %[s][c][n.m]f[e]
 
     c   \  (la)tex
@@ -42,12 +47,13 @@ http://adsabs.harvard.edu/abs_doc/help_pages/customformat.html
 
 Do this in two tabs running classic ADS
 ---------------------------------------
-Databases to query: select all three: Astronomy , Physics,  arXiv e-prints   
+Databases to query: select all three: Astronomy , Physics,  arXiv e-prints
+by default Physics is skipped.
 
 Authors:   Teuben
 
-1st. run:  All refereed articles
-2nd  run:  All non-refereed publications
+1st. tab:  All refereed articles
+2nd  tab:  All non-refereed publications
 
 
 SEND QUERY
@@ -61,9 +67,9 @@ email to:    teuben@gmail.com
 
  
 still needed a few manual edits:
+- <NUMPAGES> sometimes is used, remove them, use the pp. notation
 
 
-<NUMPAGES> sometimes is used, remove them, use the pp. notation
 
 Some additional cleanup will always be needed. I've found some araXiv entry that had also a paper entry.
 
@@ -73,4 +79,27 @@ all the \items's go between
 \begin{enumerate}[resume,label=\textbf{\arabic*}.]
 \end{enumerate}
 
+
 I decided to keep the books without numbering (historic), so those are itemize , instead of enumerate
+
+
+again, with custom format:  %% %zn %W \n \\item \n %\5.1L %\Y, ``%\T'', %\J, %\({\\bf )V\(} ), %\p-%\P. [{\\bf %c cit.}]
+
+you get something like this. I added a ordinal (the %zn) and artile type as a latex comment ahead of the entry.
+
+% 2 ARTICLE  
+\item  
+Teuben, P.~J.~and Sanders, R.~H.\ 1985,  ``Dynamical rules for barred 
+spiral galaxies'', Monthly Notices of the Royal Astronomical Society,  {\bf 
+212} , 257-273.\  [{\bf 76 cit.}] 
+
+% 15 ARTICLE  
+\item  
+Welch, W.~J., and 36 colleagues.\  1996,  "The 
+Berkeley-Illinois-Maryland-Association Millimeter Array", Publications of 
+the Astronomical Society of the Pacific,  {\bf 108} , 93- [{\bf 141 cit.}] 
+
+
+The things missing are
+
+- bold-face your name {\bf Teuben, P~J.}
